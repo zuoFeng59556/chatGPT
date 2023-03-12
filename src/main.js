a2faabbe7eac3e59520a4a48cfe5fea6ec545312
左风 <595563214@qq.com>
2023-03-12 14:42:45 +0800

    feat: 增加 markdown适配，代码高亮，还有输入框美化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css' //样式
+ 
 
 
 const app = createApp(App)
+
+//创建v-highlight全局指令
+app.directive('highlight',function (el) {
+    let blocks = el.querySelectorAll('pre code');
+    blocks.forEach((block)=>{
+      hljs.highlightBlock(block)
+    })
+  })
 
 app.use(router)
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,13 +1,16 @@
-<script lang="ts" setup>
+<script setup>
 import { ref } from "vue";
 import { Cloud } from "laf-client-sdk";
+// 将marked 引入
+import { marked } from "marked";
 
 const cloud = new Cloud({
   baseUrl: "https://jyf6wk.laf.dev",
   getAccessToken: () => "",
+  timeout: 60000,
 });
 
-const list: any = ref([]);
+const list = ref([]);
 const question = ref("");
 const parentMessageId = ref("");
 const loading = ref(false);
@@ -25,17 +28,40 @@ async function send() {
   const message = question.value;
   question.value = "";
   loading.value = true;
-
   let res;
-  if (!parentMessageId.value) {
-    res = await cloud.invoke("send", { message });
-  } else {
-    res = await cloud.invoke("send", { message, parentMessageId: parentMessageId.value });
+  if (message == "") {
+    loading.value = false;
+    list.value.push({
+      text: "问题不能为空！",
+      avatar: "/log.png",
+    });
+    setScreen();
+    return;
+  }
+  try {
+    if (!parentMessageId.value) {
+      res = await cloud.invoke("send", { message });
+    } else {
+      res = await cloud.invoke("send", {
+        message,
+        parentMessageId: parentMessageId.value,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    loading.value = false;
+    list.value.push({
+      text: "出错了，请重试！",
+      avatar: "/log.png",
+    });
+    setScreen();
+    return;
   }
 
   parentMessageId.value = res.id;
 
-  res.text = res.text.replace(/\n/g, "<br>");
+  // res.text = res.text.replace(/\n/g, "<br>");
+  res.text = marked.parse(res.text);
 
   list.value.push({
     text: res.text,
@@ -62,7 +88,7 @@ function setScreen() {
     <div id="myList">
       <div :class="item.type === 0 ? 'problemList' : 'answerList'" v-for="item in list">
         <img class="listImg" :src="item.avatar" alt="" />
-        <div v-html="item.text" class="listText"></div>
+        <div v-highlight v-html="item.text" class="listText"></div>
       </div>
 
       <div v-show="loading" class="answerList">
@@ -151,53 +177,40 @@ function setScreen() {
     <div class="steppingstone"></div>
 
     <div class="inputbox">
-      <div class="relative flex h-full flex-1 md:flex-col">
-        <div
-          class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
-        >
-          <textarea
-            v-show="!loading"
-            @keyup.enter="send"
-            tabindex="0"
-            data-id="root"
-            rows="1"
-            v-model="question"
-            id="message"
-            class="text m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"
-          ></textarea>
-          <!-- // 这里是禁用时展示 -->
-          <textarea
-            v-show="loading"
-            readonly
-            tabindex="0"
-            data-id="root"
-            rows="1"
-            v-model="question"
-            id="message"
-            class="text m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"
-          ></textarea>
-
-          <button
-            id="submit-btn"
-            @click="send"
-            class="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-5 md:bottom-0 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent"
+      <input
+        v-bind:readonly="loading"
+        @keyup.enter="send"
+        tabindex="0"
+        data-id="root"
+        rows="1"
+        v-model="question"
+        type="text"
+        id="message"
+        placeholder="输入你的指令"
+        maxlength="100"
+      />
+      <div class="btn-send" id="submit-btn" @click="send">
+        <div class="send-view" style="display: flex">
+          <svg
+            stroke="currentColor"
+            fill="none"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-4 w-4 mr-1"
+            height="1.5em"
+            width="1.5em"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              stroke="currentColor"
-              fill="none"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-4 w-4 mr-1"
-              height="1.5em"
-              width="1.5em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line x1="22" y1="2" x2="11" y2="13"></line>
-              <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-            </svg>
-          </button>
+            <line x1="22" y1="2" x2="11" y2="13"></line>
+            <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+          </svg>
+        </div>
+        <div class="send-loading" style="display: none">
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </div>
     </div>
@@ -209,35 +222,38 @@ function setScreen() {
   position: relative;
   height: 100vh;
 }
-.inputbox {
-  position: fixed;
-  width: 100%;
-  height: 150px;
-  bottom: 0px;
-  z-index: 9;
-}
+
 .defbut {
   position: fixed;
   right: 2px;
   bottom: 152px;
 }
-#submit-btn {
-  position: fixed;
-  right: 15px;
-  bottom: 65px;
-  background-color: #fff;
+.btn-send {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 48px;
+  height: 32px;
+  border-radius: 6px;
+  color: rgba(0, 0, 0, 0.6);
+  background: rgba(0, 0, 0, 0.1);
+}
+.btn-send:hover {
+  cursor: pointer;
+  opacity: 0.85;
 }
 .text {
   position: absolute;
   top: 50px;
   border: 1px solid #e5e5e5;
-  height: 40px;
+  height: 60px;
   padding: 10px;
+  width: 90%;
 }
 
 #myList {
-  width: 100%;
-  margin: auto;
+  max-width: 1000px;
+  margin: 0 auto;
   overflow-x: hidden;
   overflow-y: auto;
 }
@@ -248,8 +264,12 @@ function setScreen() {
 }
 
 .answerList {
+  position: relative;
+  padding: 20px 18px;
+  font-size: 15px;
   display: flex;
-  padding: 0px 10%;
+  overflow-x: auto;
+  white-space: pre-wrap;
   border-top: 1px solid #e5e5e5;
   border-bottom: 1px solid #e5e5e5;
 }
@@ -258,11 +278,62 @@ function setScreen() {
   margin-top: 5px;
   width: 40px;
   height: 40px;
+  border-radius: 50%;
 }
 
 .listText {
-  line-height: 50px;
   margin-left: 20px;
+  padding-top: 10px;
+  width: 100%;
+  white-space: pre-wrap;
+}
+
+.inputbox {
+  position: fixed;
+  bottom: 30px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 90%;
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding-right: 12px;
+  background: #fff;
+  border-radius: 8px;
+}
+
+.inputbox input {
+  flex-grow: 1;
+  height: 44px;
+  max-height: 100px;
+  border: 0;
+  outline: none;
+  padding: 12px 15px;
+  background: transparent;
+  font-size: 16px;
+  width: 100%;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.7);
+}
+
+.inputbox button {
+  margin-left: 15px;
+  width: 56px;
+  height: 82%;
+  border-radius: 6px;
+  border: 0;
+  background: silver;
+  color: #333;
+  font-size: 14px;
+  outline: none;
+}
+
+.inputbox button:hover {
+  cursor: pointer;
+  opacity: 0.8;
 }
 
 .addin {
@@ -278,7 +349,7 @@ function setScreen() {
 
 .begintitle {
   width: 100%;
-  padding: 50px 50px 30px 50px;
+  /* padding: 50px 50px 30px 50px; */
 }
 .begintitle h1 {
   padding: 50px;
@@ -319,25 +390,13 @@ textarea {
 }
 
 @media screen and (max-width: 600px) {
-  .inputbox {
-    position: fixed;
-    width: 100%;
-    height: 100px;
-    bottom: 0px;
-    z-index: 9;
-  }
   .text {
     position: absolute;
     top: 30px;
     border: 1px solid #e5e5e5;
-    height: 40px;
+    height: 45px;
     padding: 10px;
-  }
-  #submit-btn {
-    position: fixed;
-    right: 15px;
-    bottom: 35px;
-    background-color: #fff;
+    width: 80%;
   }
 }
 </style>
