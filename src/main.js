e0829cc8ef173a05f593503ce2b16dc551bf72be
左风 <595563214@qq.com>
2023-03-20 17:00:57 +0800

    fix:

--- src/main.js
+++ src/main.js
@@ -4,19 +4,21 @@ import router from './router'
 import './index.css'
 import hljs from 'highlight.js';
 import 'highlight.js/styles/atom-one-dark.css' //样式
- 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 
 const app = createApp(App)
 
 //创建v-highlight全局指令
-app.directive('highlight',function (el) {
-    let blocks = el.querySelectorAll('pre code');
-    blocks.forEach((block)=>{
-      hljs.highlightBlock(block)
-    })
+app.directive('highlight', function (el) {
+  let blocks = el.querySelectorAll('pre code');
+  blocks.forEach((block) => {
+    hljs.highlightBlock(block)
   })
+})
 
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
