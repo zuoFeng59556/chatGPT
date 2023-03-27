07e0c43a570b8564ec7702b6f394e524964c7246
imelonkid <imelonkid@163.com>
2023-03-27 23:22:35 +0800

    youhua优化 quchu去除chongzhi充值dengl登录

--- src/views/index.vue
+++ src/views/index.vue
@@ -9,7 +9,7 @@ import QrcodeVue from "qrcode.vue";
 import { ElMessage } from "element-plus";
 
 const cloud = new Cloud({
-  baseUrl: "https://jyf6wk.laf.dev",
+  baseUrl: "https://t2eap0.laf.dev",
   getAccessToken: () => localStorage.getItem("access_token"),
   timeout: 60000,
 });
@@ -60,9 +60,6 @@ const tel = /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-
 
 //======================================created======================================
 
-// 获取用户剩余次数
-getAmount();
-
 // 判断是否为移动设备
 if (
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -76,72 +73,10 @@ if (
 
 //======================================function======================================
 
-// 获取用户剩余次数
-async function getAmount() {
-  if (!localStorage.getItem("access_token")) return;
-  const res = await cloud.invoke("get-amount");
-  amount.value = res.amount;
-}
 
-//获取验证码
-async function getCode() {
-  if (!tel.test(phone.value)) return (err.value = true);
-  if (codebut.value) return;
-  const res = await cloud.invoke("getCode", { phone: phone.value });
-  countDown();
-}
-
-//验证码倒计时
-function countDown() {
-  if (!canClick.value) return;
-  codebut.value = true;
-  canClick.value = false;
-  content.value = totalTime.value + "s后重新发送";
-  let clock = window.setInterval(() => {
-    totalTime.value--;
-    content.value = totalTime.value + "s后重新发送";
-    if (totalTime.value < 0) {
-      window.clearInterval(clock);
-      content.value = "重新发送验证码";
-      totalTime.value = 60;
-      canClick.value = true; //这里重新开启
-      codebut.value = false;
-    }
-  }, 1000);
-}
-
-//验证登录
-async function login() {
-  const res = await cloud.invoke("login", { phone: phone.value, code: code.value });
-  console.log(res);
-  if (res.code === 1) {
-    localStorage.setItem("access_token", res.data.access_token);
-    localStorage.setItem("user", JSON.stringify(res.data.user));
-    success();
-    centerDialogVisible2.value = false;
-    getAmount();
-  } else {
-    ElMessage({
-      message: "无效的验证码",
-      type: "error",
-    });
-  }
-}
 
 //发送消息
 async function send() {
-  //发送时验证登录
-  if (localStorage.getItem("access_token") == null)
-    return ElMessage({
-      message: "请先登录！",
-      type: "error",
-    });
-  //判断用户次数
-  if (amount.value <= 0)
-    return ElMessage({
-      message: "您的剩余次数不足，请充值！",
-      type: "error",
-    });
   //判断是否回复
   if (loading.value) return;
   list.value.push({
@@ -193,7 +128,6 @@ async function send() {
   });
 
   loading.value = false;
-  getAmount();
   setScreen();
 }
 
@@ -204,46 +138,9 @@ function setScreen() {
   }, 0);
 }
 
-//验证手机号码弹出框
-function close() {
-  err.value = false;
-}
-
 //判断用户选择项
 function select(e) {
   indexUp.value = e;
-}
-
-//点击充值
-async function openCode() {
-  let num = 0;
-  if (indexUp.value == 0) num = 2000;
-  if (indexUp.value == 1) num = 5000;
-  if (indexUp.value == 2) num = 100000;
-
-  const res = await cloud.invoke("pay", { amount: num });
-  payOrder.value = res.orderId;
-  codeUrl.value = res.codeUrl;
-  centerDialogVisible.value = false;
-  upCode.value = true;
-  checkPay();
-}
-
-//验证用户是否付款
-async function checkPay() {
-  const res = await cloud.invoke("check-pay-ordet", { order: payOrder.value });
-  if (res.code == 1) {
-    upCode.value = false;
-    getAmount();
-    ElMessage({
-      message: "充值成功",
-      type: "success",
-    });
-  } else {
-    setTimeout(() => {
-      checkPay();
-    }, 1000);
-  }
 }
 
 //发送消息适配PC或phone
@@ -253,41 +150,12 @@ function handleEnter(e) {
   }
 }
 
-//消息弹出框
-const success = () => {
-  ElMessage({
-    message: "登录成功",
-    type: "success",
-  });
-};
-
-//判断是否登录
-function judge() {
-  const access_token = localStorage.getItem("access_token");
-  if (access_token)
-    return ElMessage({
-      message: "您已经登录过了",
-      type: "success",
-    });
-  centerDialogVisible2.value = true;
-}
-
-//登录后可点击充值
-function judgeUp() {
-  if (!localStorage.getItem("access_token"))
-    return ElMessage({
-      message: "请登录",
-      type: "error",
-    });
-  centerDialogVisible.value = true;
-}
 </script>
 
 <template>
   <div class="page">
     <!-- 头部 -->
     <el-row class="head">
-      <div class="amount">剩余{{ amount }}</div>
       <div>
         <el-col :span="24">
           <!-- ------------------------------------------ -->
@@ -299,15 +167,15 @@ function judgeUp() {
           </el-popover>
           <!-- ------------------------------------------ -->
 
-          <el-button @click="judgeUp">充值</el-button>
+          <!-- <el-button @click="judgeUp">充值</el-button> -->
           <!-- ------------------------------------------ -->
 
-          <el-button @click="judge">
+          <!-- <el-button @click="judge">
             <el-icon style="vertical-align: middle">
               <User />
             </el-icon>
             <span style="vertical-align: middle">登录</span>
-          </el-button>
+          </el-button> -->
           <!-- ------------------------------------------ -->
         </el-col>
       </div>
@@ -315,112 +183,6 @@ function judgeUp() {
 
     <!-- 占位 -->
     <div style="height: 52px"></div>
-
-    <!-- 充值弹出框 -->
-    <el-dialog v-model="centerDialogVisible" title="充值" width="50%" height="50%" center>
-      <div class="cardbox">
-        <!-- --------------------------------------------------------------- -->
-        <el-card @click="select(0)" :class="indexUp === 0 ? 'box-card' : 'boxCard'">
-          <div class="useNumber">400次</div>
-          <div class="money">
-            <span class="sign">￥</span>
-            <span class="number">20</span>
-          </div>
-        </el-card>
-
-        <!-- --------------------------------------------------------------- -->
-
-        <el-card
-          @click="select(1)"
-          :class="indexUp === 1 ? 'box-card' : 'boxCard'"
-          class="box-card"
-        >
-          <div class="useNumber">1千次</div>
-          <div class="money">
-            <span class="sign">￥</span>
-            <span class="number">50</span>
-          </div>
-        </el-card>
-
-        <el-card
-          @click="select(2)"
-          :class="indexUp === 2 ? 'box-card' : 'boxCard'"
-          class="box-card"
-        >
-          <div class="useNumber">2万次</div>
-          <div class="money">
-            <span class="sign">￥</span>
-            <span class="number">1000</span>
-          </div>
-        </el-card>
-      </div>
-      <!-- ----------------------------------------------------- -->
-      <div class="cheerbox">
-        <el-button @click="openCode" class="cheer" type="warning">充值</el-button>
-      </div>
-      <template #footer> </template>
-    </el-dialog>
-
-    <!-- 二维码弹出框 -->
-    <el-dialog v-model="upCode" width="50%" height="50%" center>
-      <div class="qrcode">
-        <qrcode-vue :value="codeUrl" :size="300" level="H" />
-        <div class="qrcodeText">微信扫码支付</div>
-      </div>
-      <template #footer> </template>
-    </el-dialog>
-
-    <!-- 登录弹出框 -->
-    <el-dialog v-model="centerDialogVisible2" title="登录" center>
-      <el-alert
-        @close="close"
-        v-show="err"
-        title="请输入正确的手机号码"
-        type="error"
-        center
-        show-icon
-      />
-      <div style="height: 200px">
-        <!-- ----------------------------------------------------- -->
-        <div class="accountbox">
-          <div class="inputname">手机号：</div>
-          <el-input
-            class="elinput"
-            size="small"
-            type="number"
-            v-model="phone"
-            placeholder="请输入手机号"
-          />
-        </div>
-
-        <!-- ----------------------------------------------------- -->
-
-        <div class="accountbox">
-          <div class="inputname">验证码：</div>
-          <el-input
-            class="elinputcode"
-            size="small"
-            v-model="code"
-            placeholder="请输入验证码"
-          />
-        </div>
-        <!-- ----------------------------------------------------- -->
-
-        <div class="loginbutbox">
-          <el-button
-            @click="getCode"
-            type="primary"
-            style="margin-top: 10px"
-            class="loginbut"
-            >{{ content }}</el-button
-          >
-        </div>
-        <div class="loginbutbox">
-          <el-button @click="login" class="loginbut">登录</el-button>
-        </div>
-      </div>
-      <template #footer> </template>
-    </el-dialog>
 
     <!-- ------------------------------------------------------------ -->
     <div v-show="!list.length" class="begintitle">
