c0478c4032a698f53ae4854b2c462659ef2640ee
左风 <595563214@qq.com>
2023-06-09 07:11:28 +0800

    fix：删除tw报错

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
-  // tailwind.config.js
-  module.exports = {
-    purge: [],
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+// tailwind.config.js
+module.exports = {
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
